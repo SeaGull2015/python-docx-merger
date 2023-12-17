5ec73efa6fd0a09e7ff6e2c723aa210427211aaf
SeaGull2104 <wk.zaytsev.dv@gmail.com>
2023-12-17 13:16:40 +0300

    Added errors

--- main.py
+++ main.py
@@ -12,7 +12,15 @@ composer = Composer(master)
 
 for file in todo:
     path = os.path.abspath("")+"\docxfiles\\"+file
-    docfile = Document(path)
-    composer.append(docfile)
+    try:
+        docfile = Document(path)
+    except:
+        print("error reading the document at " + path)
+        exit(2)
+    try:
+        composer.append(docfile)
+    except:
+        print("error appending the document at " + path)
+        exit(1)
 
 composer.save(os.path.abspath("")+"\docxfiles\combined.docx")
